6ed3fd018fbab7a960d56f8b8bec96fe4e1d62f3
cai <429546420@qq.com>
2020-11-22 23:31:24 +0800

    add more solution for 509_fibonacci_number

--- Python/Leetcodes/Array/509_fibonacci_number.py
+++ Python/Leetcodes/Array/509_fibonacci_number.py
@@ -13,19 +13,29 @@ https://leetcode-cn.com/problems/fibonacci-number/
 509题--斐波那契数
 """
 
-
 class Solution:
     def fib(self, N: int) -> int:
         if N <= 1:
             return N
         a, b = 0, 1
-        for i in range(2, N+1):
+        for i in range(2, N + 1):
             a, b = b, a + b
         return b
+
+    def fib2(self, N: int) -> int:
+        a = [0, 1]
+        for i in range(N - 1):
+            a.append(a[-1] + a[-2])
+        return a[N]
+
+    def fib3(self, N: int) -> int:
+        if N <= 1:
+            return N
+        return self.fib(N - 1) + self.fib(N - 2)
 
 
 if __name__ == '__main__':
     n = 4
     print(f"n={n}")
     solution = Solution()
-    print("result: ", solution.fib(n))
+    print("result: ", solution.fib3(n))
